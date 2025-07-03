f495c336fbab3acd94f322ea3b3fe1c4092b4106
f-debrandois <felix.debrandois@gmail.com>
2025-07-03 09:27:41 +0700

    Rename2

--- S9/Statistiques/TP6/solutions/scratch/vocab_size.py
+++
@@ -1,5 +0,0 @@
-vocab = list(set([w for text in train_data.keys() for w in text.split(' ')]))
-vocab_size = len(vocab)
-
-print('Vocabulary:',vocab)
-print('--> %d unique words' % vocab_size)
--- S9/Statistiques/TP9/solutions/vae/vae.py
+++
@@ -1,4 +0,0 @@
-# instantiate VAE model
-outputs = decoder(encoder(inputs))
-vae = km.Model(inputs, outputs, name='vae_mlp')
-vae.summary()
